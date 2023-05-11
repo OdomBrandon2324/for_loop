81bdb9f147cc27feaa19aedf4612f82003df2445
Brandon Odom <120235400+OdomBrandon2324@users.noreply.github.com>
2023-05-10 19:05:00 -0700

    Add files via upload

---
+++ for_loop_basic1.py/for_loop_basic1.py
@@ -0,0 +1,29 @@
+# for i in range(151):
+#     print(i)
+
+# for i in range(5, 1001, 5):
+#     print(i)
+
+# for i in range(1,101):
+#     if i % 10 == 0:
+#         print("Coding Dojo")
+#     elif i % 5 == 0:
+#         print("Coding")
+#     else:
+#         print(i)
+
+# sum = 0
+# for i in range(0, 500001):
+#     if i % 2 != 0:
+#         sum += i
+# print("Sum of odd integers from 0 to 500,000:")
+
+# num = 2018
+# while num > 0:
+#     print(num)
+#     num -= 4
+
+
+for i in range(lowNum, highNum+1):
+    if i % mult == 0:
+        print(i)
